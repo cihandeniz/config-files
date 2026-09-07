9bd2f81af1bd8a84a8ec6928c6b0d6c9026dbefc
cihandeniz <cihan@gcbrains.com>
2026-09-07 13:26:26 +0300

    update rename in vue files

--- nvim/init.lua
+++ nvim/init.lua
@@ -297,6 +297,21 @@ require("lazy").setup({
     config = function()
       vim.lsp.config("vtsls", {
         filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
+        settings = {
+          vtsls = {
+            tsserver = {
+              globalPlugins = {
+                {
+                  name = "@vue/typescript-plugin",
+                  location = vim.fn.stdpath("data")
+                    .. "/mason/packages/vue-language-server/node_modules/@vue/language-server",
+                  languages = { "vue" },
+                  configNamespace = "typescript",
+                },
+              },
+            },
+          },
+        },
       })
       vim.lsp.config("eslint", {})
       vim.lsp.config("cssls",  {})
